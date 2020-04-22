1d6512aaa663c3a22425ed2486d1e8efc3e22e9b
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2020-04-22 17:04:56 -0400

    still working on main layout

--- src/componenets/Header/index.js
+++ src/componenets/Header/index.js
@@ -5,7 +5,8 @@ function Header() {
     return <div>
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4 row pageHeader">Austin Atkinson Portfolio</h1>
+                <h1 class="display-4 justify-content-center row pageHeader">Austin Atkinson Portfolio</h1>
+                <img class="row justify-content-center headshot" src="assets/Images/IMG_2668.JPG" alt="Image did not load"></img>
             </div>
         </div>
     </div>
